d17499ffc276798e8347a1815b177c932dda9e7d
rojojun <zoloman316@gmail.com>
2025-08-01 23:07:28 +0900

    JPA 클래스 추상화 정리

--- src/main/java/com/rojojun/splearn/domain/AbstractEntity.java
+++ src/main/java/com/rojojun/splearn/domain/AbstractEntity.java
@@ -6,10 +6,24 @@ import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 
+import java.util.Objects;
+
 @Getter
 @MappedSuperclass
 public abstract class AbstractEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractEntity that = (AbstractEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
